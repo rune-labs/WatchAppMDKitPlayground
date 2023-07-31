eeeb5106acdccd651d4bf5ff41a1d8152f25f5fe
brucerune <bruce@runelabs.io>
2023-07-31 15:25:27 -0700

    Initial Commit

---
+++ CrashRepro Watch App/CrashReproApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrashReproApp.swift
+//  CrashRepro Watch App
+//
+//  Created by Bruce Daniel Rune Labs on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct CrashRepro_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CrashRepro/CrashReproApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrashReproApp.swift
+//  CrashRepro
+//
+//  Created by Bruce Daniel Rune Labs on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct CrashReproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

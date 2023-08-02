5dbc5d9f36b826d8a747e298e997c5fc55a81962
brucerune <bruce@runelabs.io>
2023-08-02 13:40:24 -0700

    hookup api

--- CrashRepro/CrashRepro Watch App/CrashReproApp.swift
+++ CrashRepro/CrashRepro Watch App/CrashReproApp.swift
@@ -12,6 +12,7 @@ import CoreMotion
 
 @main
 struct CrashRepro_Watch_AppApp: App {
+    let disorderManager = CMMovementDisorderManager()
     var body: some Scene {
         WindowGroup {
             ContentView()
@@ -19,16 +20,17 @@ struct CrashRepro_Watch_AppApp: App {
     }
     
     init() {
-        // disorderManager.monitorKinesias(forDuration: 60.0 * 60.0 * 24.0 * 7.0)
+         disorderManager.monitorKinesias(forDuration: 60.0 * 60.0 * 24.0 * 7.0)
         print("identifierForVendor: \(String(describing: WKInterfaceDevice.current().identifierForVendor?.uuidString))")
-        print("SKPaymentQueue.countryCode: \(SKPaymentQueue.default().storefront?.countryCode)")
-        // print("CMMovementDisorderManager.authorizationStatus(): \(CMMovementDisorderManager.authorizationStatus())")
-        // print("monitorKinesiasExpirationDate: \(String(describing: disorderManager.monitorKinesiasExpirationDate()))")
-        // print("lastProcessedDate: \(disorderManager.lastProcessedDate())")
-//        disorderManager.queryDyskineticSymptom(from: Date(),
-  //                                                    to: Date()) { (dyskineticSymptomResult, error) in
-    //        print("queryDyskineticSymptom.dyskineticSymptomResult: \(dyskineticSymptomResult)")
-      //      print("queryDyskineticSymptom.error: \(String(describing: error))")
-        //}
+        print("SKPaymentQueue.countryCode: \(String(describing: SKPaymentQueue.default().storefront?.countryCode))")
+        print("CMMovementDisorderManager.authorizationStatus(): \(CMMovementDisorderManager.authorizationStatus())")
+        print("monitorKinesiasExpirationDate: \(String(describing: disorderManager.monitorKinesiasExpirationDate()))")
+        print("lastProcessedDate: \(disorderManager.lastProcessedDate())")
+        disorderManager.queryDyskineticSymptom(
+            from: Date(),
+            to: Date()) { (dyskineticSymptomResult, error) in
+            print("queryDyskineticSymptom.dyskineticSymptomResult: \(dyskineticSymptomResult)")
+            print("queryDyskineticSymptom.error: \(String(describing: error))")
+        }
     }
 }

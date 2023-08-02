331fd8ce0c7acdb8ede9916f7b89d4c371b2b21a
brucerune <bruce@runelabs.io>
2023-08-02 14:14:05 -0700

    setup callbacks

--- CrashRepro/CrashRepro Watch App/CrashReproApp.swift
+++ CrashRepro/CrashRepro Watch App/CrashReproApp.swift
@@ -12,25 +12,44 @@ import CoreMotion
 
 @main
 struct CrashRepro_Watch_AppApp: App {
+    @Environment(\.scenePhase) private var scenePhase
     let disorderManager = CMMovementDisorderManager()
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Group {
+                ContentView()
+            }
+            .onChange(of: scenePhase, perform: { value in
+                DispatchQueue.global().async {
+                    switch value {
+                    case .active:
+                        testAPIs()
+                    case .background:
+                        break
+                    case .inactive:
+                        break
+                    @unknown default:
+                        break
+                    }
+                }
+            })
         }
     }
     
-    init() {
-         disorderManager.monitorKinesias(forDuration: 60.0 * 60.0 * 24.0 * 7.0)
-        print("identifierForVendor: \(String(describing: WKInterfaceDevice.current().identifierForVendor?.uuidString))")
-        print("SKPaymentQueue.countryCode: \(String(describing: SKPaymentQueue.default().storefront?.countryCode))")
-        print("CMMovementDisorderManager.authorizationStatus(): \(CMMovementDisorderManager.authorizationStatus())")
-        print("monitorKinesiasExpirationDate: \(String(describing: disorderManager.monitorKinesiasExpirationDate()))")
-        print("lastProcessedDate: \(String(describing: disorderManager.lastProcessedDate()))")
-        disorderManager.queryDyskineticSymptom(
-            from: Date(),
-            to: Date()) { (dyskineticSymptomResult, error) in
-            print("queryDyskineticSymptom.dyskineticSymptomResult: \(dyskineticSymptomResult)")
-            print("queryDyskineticSymptom.error: \(String(describing: error))")
-        }
+    func testAPIs() {
+        disorderManager.monitorKinesias(forDuration: 60.0 * 60.0 * 24.0 * 7.0)
+       print("identifierForVendor: \(String(describing: WKInterfaceDevice.current().identifierForVendor?.uuidString))")
+       print("SKPaymentQueue.countryCode: \(String(describing: SKPaymentQueue.default().storefront?.countryCode))")
+       print("CMMovementDisorderManager.authorizationStatus(): \(CMMovementDisorderManager.authorizationStatus())")
+       print("monitorKinesiasExpirationDate: \(String(describing: disorderManager.monitorKinesiasExpirationDate()))")
+       print("lastProcessedDate: \(String(describing: disorderManager.lastProcessedDate()))")
+       if let lastProcessed = disorderManager.lastProcessedDate() {
+           disorderManager.queryDyskineticSymptom(
+               from: lastProcessed.addingTimeInterval(-60.0 * 60.0 * 3.0),
+               to: lastProcessed) { (dyskineticSymptomResult, error) in
+               print("queryDyskineticSymptom.dyskineticSymptomResult: \(dyskineticSymptomResult)")
+               print("queryDyskineticSymptom.error: \(String(describing: error))")
+           }
+       }
     }
 }
